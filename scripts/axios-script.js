4b2cc8a6b3b670124f7d78ee175fe58acc8601dc
Jad Yahya <jadoyahya@gmail.com>
2022-09-11 23:43:23 +0300

    Added bored function

--- scripts/axios-script.js
+++ scripts/axios-script.js
@@ -1,8 +1,15 @@
 const ip = document.getElementById("ip");
+const boredBtn = document.querySelector(".bored-btn");
 
 const ipOptions = {
   method: "GET",
   url: "https://api.ipify.org/",
+  params: { format: "json" },
+};
+
+const boredOptions = {
+  method: "GET",
+  url: "https://www.boredapi.com/api/activity",
   params: { format: "json" },
 };
 
@@ -16,3 +23,16 @@ function getIp() {
       console.error(error);
     });
 }
+
+function iAmBored() {
+  axios
+    .request(boredOptions)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+boredBtn.onclick = () => iAmBored();
