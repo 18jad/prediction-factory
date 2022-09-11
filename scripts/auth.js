4099d624d41a6c4dce07c5691e07e26097054e8a
Jad Yahya <jadoyahya@gmail.com>
2022-09-11 16:49:33 +0300

    Fixed a bug manipulating all forms

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,7 +1,14 @@
-const signInForm = document.querySelector(".signin-container");
-const signUpForm = document.querySelector(".signup-container");
-const signInLink = document.getElementById("signInLink");
-const signUpLink = document.getElementById("signUpLink");
+const signInForm = document.querySelector(".signin-container"),
+  signUpForm = document.querySelector(".signup-container"),
+  signInLink = document.getElementById("signInLink"),
+  signUpLink = document.getElementById("signUpLink"),
+  signUpUsername = document.getElementById("signUpUsername"),
+  signUpPassword = document.getElementById("signUpPassword"),
+  signUpPasswordConfirmation = document.getElementById(
+    "signUpPasswordConfirmation",
+  ),
+  signInUsername = document.getElementById("signInUsername"),
+  signInPassword = document.getElementById("signInPassword");
 
 function switchAuth(type) {
   if (type == "signin") {
@@ -15,3 +22,29 @@ function switchAuth(type) {
 
 signInLink.onclick = () => switchAuth("signin");
 signUpLink.onclick = () => switchAuth("signup");
+
+function signUp() {
+  let username = signUpUsername.value,
+    password = signUpPassword.value,
+    confirmedPassword = signUpPasswordConfirmation.value;
+  if (username.trim < 3) {
+    throw new Error("Username is too short");
+  }
+  if (password != confirmedPassword) {
+    throw new Error("Passwords are not identical");
+  }
+  if (localStorage.getItem("users")) {
+    let arr = localStorage.getItem("users");
+    arr.push({ username, password });
+    localStorage.setItem("users", arr);
+  } else {
+    localStorage.setItem("users", JSON.stringify([{ username, password }]));
+  }
+}
+
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  signUp();
+});
+
+signInForm.addEventListener("submit", (e) => {});
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("form"),
+const form = document.getElementById("predictionForm"),
   nameInput = document.getElementById("nameInput"),
   resultsContainer = document.querySelector(".prediction-results"),
   predictedAge = document.getElementById("predictedAge"),
